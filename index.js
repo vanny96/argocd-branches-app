74a1912230db9149ab1e96b6d320b55c5843470a
Giovanni Barbaro <vanni-96@hotmail.it>
2023-06-14 10:54:49 +0200

    Creates second version of the app

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const enviorment = process.env["ARGOCD_BRANCHES_APP_ENV"]
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(`Hello World from ${enviorment}!`)
 })
 
 app.listen(port, () => {
